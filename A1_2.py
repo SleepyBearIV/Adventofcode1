baf26d9cf6e3694bce5e36f7f24bb94536abd359
SleepyBearIV <mattias.nicander.mn@gmail.com>
2024-02-11 17:11:22 +0100

    start of second - need to find a way to separate the text so i can convert to numbers

--- A1_2.py
+++ A1_2.py
@@ -1,11 +1,34 @@
+import re
+
 #opens the document
 f = open('input_1A.txt', 'r').read()
 #splits the teixt in to array
 with open('input_1A.txt', 'r') as file:
     text = f.split("\n")
 
+word_to_digits = {
+    'zero': '0',
+    'one': '1',
+    'two': '2',
+    'three': '3',
+    'four': '4',
+    'five': '5',
+    'six': '6',
+    'seven': '7',
+    'eight': '8',
+    'nine': '9'
+}
+word = text[0].split()
+digits = [word_to_digits[x] for x in word]
+
+print(text[0])
+print(word)
+print(digits)
+
+
+
 inputArray = text
-print(text)
+
 
 inputNumbers = []
 
