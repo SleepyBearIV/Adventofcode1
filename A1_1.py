b6e3e66411ffc6c771708c072350afb569c9764c
SleepyBearIV <mattias.nicander.mn@gmail.com>
2024-02-11 16:29:41 +0100

    done

--- A1_1.py
+++ A1_1.py
@@ -1,25 +1,41 @@
+#opens the document
 f = open('input_1A.txt', 'r').read()
-
+#splits the teixt in to array
 with open('input_1A.txt', 'r') as file:
     text = f.split("\n")
 
+inputArray = text
+print(text)
 
-CodeString = text
-print(CodeString)
+inputNumbers = []
 
 def extract_nums(string):
     nums = []
 
     for char in string:
         if char.isnumeric():
-            num = int(char)
-            nums.append(num)
-    print(nums)
-    return nums
+            if len(nums) > 1:
+                nums.pop()
+                num = int(char)
+                nums.append(num)
+            else:
+                num = int(char)
+                nums.append(num)
+                nums.append(num)
+    s = [str(i) for i in nums]
+    number = int("".join(s))
+    return number
 
-
-for x in CodeString:
+for x in inputArray:
     extract_nums(x)
+    inputNumbers.append(extract_nums(x))
+
+sum = 0
+
+for i in inputNumbers:
+    sum = sum + i
+print(sum)
+
 
 
 
