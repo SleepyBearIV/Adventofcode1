865a08e3c4d774ac44afe138d7dafb88c03ab549
SleepyBearIV <mattias.nicander.mn@gmail.com>
2024-02-16 16:02:55 +0100

    second try sucsess, need to find out why 'text' in my funktion is neesesary

--- A1_2.py
+++ A1_2.py
@@ -3,8 +3,10 @@
 with open('input_1A.txt', 'r') as file:
     inputArray = file.read().split("\n")
 
+inputNumbers = []
+inputArray_numbers = []
 
-def text_to_numbers(text):
+def text_to_numbers(string):
     # Define a dictionary to map text representations of numbers to their numerical values
     number_dict = {
         'zero': '0',
@@ -21,11 +23,38 @@ def text_to_numbers(text):
 
     # Replace textual representations with numerical values
     for word, number in number_dict.items():
-        text = text.replace(word, number)
-    return text
+        string = string.replace(word, number)
+    return string
+
+inputArray_numbers = []
 
 for i in inputArray:
     text_to_numbers(i)
     inputArray_numbers.append(text_to_numbers(i))
 
-print(inputArray_numbers)
+def extract_nums(string):
+    nums = []
+
+    for char in string:
+        if char.isdigit():
+            if len(nums) > 1:
+                nums.pop()
+                num = int(char)
+                nums.append(num)
+            else:
+                num = int(char)
+                nums.append(num)
+                nums.append(num)
+    number = int("".join(str(i) for i in nums))
+    return number
+
+for x in inputArray_numbers:
+    extract_nums(x)
+    inputNumbers.append(extract_nums(x))
+
+
+sum = 0
+print(inputNumbers)
+for i in inputNumbers:
+    sum = sum + i
+print(sum)
